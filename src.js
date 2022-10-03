fd87e8fa55c72538f001f00be723cf632a3925ac
Sara Hakobyan <sarah.hakobyan@gmail.com>
2022-10-03 16:35:56 +0400

    unnecessay code deleted

--- src.js
+++ src.js
@@ -7,16 +7,3 @@ const board = mySudoku.checkedNums
 const gameBoard = mySudoku.gameBoard
 
 
-function print(value) {
-  let numbers = ''
-  for (i = 0; i < value.length; i++) {
-    if (i % 9 === 0) {
-      numbers += '\n'
-    }
-    numbers += value[i].num + "  "
-  }
-  console.log(numbers)
-}
-
-print(board);
-print(gameBoard) 
