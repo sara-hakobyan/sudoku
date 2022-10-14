6e0e482e545734151f3af5260ad6ca21b656f09d
Sara Hakobyan <sarah.hakobyan@gmail.com>
2022-10-14 17:34:45 +0400

    new functions added in someclass.js file

--- someClass.js
+++ someClass.js
@@ -1,40 +1,76 @@
- class Board {
-    constructor (){
+
+
+const HTMLUtils = {
+    createBoard: function(id) {
+        let board = document.getElementById(id)
+        return board
+    },
+
+    createCell: function(cssClass) {
+        let boardCell = document.createElement('div')
+        boardCell.className = cssClass
+        return  boardCell
+    },
+
+    collectionOfCells: function(cssClass) {
+        let buttons = document.querySelectorAll(cssClass)
+        return buttons
+    }
+}
+
+
+
+class Board {
+    constructor (parentContainerId){
+        this.parentContainerId = parentContainerId
+        this.onNumberClick = this.onNumberClick.bind(this)
     }
     
-    getBoard(id) {
-        let board = document.createElement('div')
-        board.setAttribute('id', id)
-        document.body.appendChild(board)
+     
+    createBoard(value) {
+        let board = HTMLUtils.createBoard(this.parentContainerId)
+        let cell = HTMLUtils.createCell('clickable-number')
+        cell.innerHTML = value
+        board.appendChild(cell)
+        let boardCells = HTMLUtils.collectionOfCells('.clickable-number')
+        for (let i = 0; i < boardCells.length; i++) {
+            boardCells[i].addEventListener('click', this.onNumberClick, {once: true})
+            // boardCells[i].removeEventListener('click', this.onNumberClick)
+        }
+        console.log(cell)                                              
+        return boardCells
+    }
+    
+  
+    update(numbers) {
+        console.log('njn jn', this.boardCells)
+        for (let i = 0; i < numbers.length; i++) {
+            board = this.createBoard(numbers[i])
+        }
         return board
     }
-    
-    createCell() {
-        let boardCell = document.createElement('div')
-        return  boardCell
-    }
-
-    update(numbers) {
-        let board = this.getBoard('board')
-        for (let i = 0; i < numbers.length; i++) {
-            let boardCell = this.createCell()
-            boardCell.innerHTML = numbers[i]
-            boardCell.addEventListener('click', () => this.onNumberClick(numbers[i]))
-            board.appendChild(boardCell)
-            console.log(boardCell)
-        }
-    }
 
 
-    onNumberClick(number) {
-        let board = this.getBoard('visibles-board')
-        let boardCell = this.createCell()
-        boardCell.innerText = number
+    onNumberClick(event) {
+        let value = event.target.innerHTML
+        let board = HTMLUtils.createBoard('visibles-board')
+        let boardCell = HTMLUtils.createCell('clicked-number')
+        boardCell.innerText = value
         board.appendChild(boardCell)
         console.log(boardCell)
     }
 
+
+    removeAll(){
+        let boardCells = HTMLUtils.collectionOfCells('.clickable-number')
+        for (let i = 0; i < boardCells.length; i++) {
+            boardCells[i].removeEventListener('click', this.onNumberClick)
+        }
+        return boardCells
+    }
+
 }
+
 
 function board() {
     let arr = []
@@ -47,6 +83,6 @@ function board() {
 let arr = board()
 
 
-const myBoard = new Board(arr)
+const myBoard = new Board('grid-board')
 // myBoard.update(arr)
 
